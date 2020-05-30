97b0309a35d89f901e373356c98d83ae034e6c0c
Rondo <you@example.com>
2020-05-30 16:51:29 -0400

    First Commit to GitHub

--- test.py
+++ test.py
@@ -1,5 +1,19 @@
-a = [1,2,3,4,5]
+import random
+a = list(range(10))
+b = [random.randint(-5,5) for x in a]
+c = random.randrange(1,10)
 
-print(a[0:4:2])
+thisIsAList = a[:]
+
 
 print("This is just a test!")
+a1 = [ x * x for x in a]
+print(a)
+print(a[0:4:2])
+print(a1)
+random.shuffle(a)
+print("Random shuffle function on 'a'",a)
+print(b)
+print(c)
+print(random.sample(range(1,100),5))
+print(thisIsAList)
